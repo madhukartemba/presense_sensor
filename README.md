dd193d4d6566524ef60f93ff0f97117e912d385a
madhukartemba <madhusmiles.madhukar@gmail.com>
2026-08-13 18:00:04 +0530

    upgrade esphome version

--- README.md
+++ README.md
@@ -5,5 +5,5 @@ It is designed for seamless integration with Home Assistant.
 
 Build commands:
 Python version 3.12
-Esphome version 2026.1.2
+Esphome version 2026.7.4
 Example: esphome -s device_id 3 run prd/presence_1_basic/presence_1_basic.yaml
--- build.py
+++ build.py
@@ -9,7 +9,7 @@ import shutil
 # --- Modified Constants ---
 VENV_DIRNAME = ".venv"
 REQUIRED_PYTHON = "python3.12"
-ESPHOME_VERSION = "2026.6.2"
+ESPHOME_VERSION = "2026.7.4"
 # --------------------------
 
 def find_project_root(start: Path) -> Path:
